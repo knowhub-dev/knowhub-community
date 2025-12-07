c83bde35a0b8939bf0ff5cada0720f11a7aad1bd
Ubuntu <ubuntu@mail.solvera.cloud>
2025-12-07 11:15:03 +0000

    fix

--- config/cors.php
+++ config/cors.php
@@ -16,19 +16,31 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => array_values(array_unique(array_merge([
-        'https://space.knowhub.uz',
-        'https://api.space.knowhub.uz',
-        'https://www.space.knowhub.uz',
-        'http://localhost',
-        'http://localhost:3000',
-        'http://127.0.0.1',
-        'http://127.0.0.1:3000',
-        'https://localhost',
-        'https://localhost:3000',
-        'https://127.0.0.1',
-        'https://127.0.0.1:3000',
-    ], array_filter(array_map('trim', preg_split('/[,\s]+/', env('CORS_ALLOWED_ORIGINS', '')))))),
+    'allowed_origins' => array_values(
+        array_unique(
+            array_merge(
+                [
+                    'https://space.knowhub.uz',
+                    'https://api.space.knowhub.uz',
+                    'https://www.space.knowhub.uz',
+                    'http://localhost',
+                    'http://localhost:3000',
+                    'http://127.0.0.1',
+                    'http://127.0.0.1:3000',
+                    'https://localhost',
+                    'https://localhost:3000',
+                    'https://127.0.0.1',
+                    'https://127.0.0.1:3000',
+                ],
+                array_filter(
+                    array_map(
+                        'trim',
+                        preg_split('/[,\s]+/', env('CORS_ALLOWED_ORIGINS', ''))
+                    )
+                )
+            )
+        )
+    ),
 
     'allowed_headers' => ['*'],
 
